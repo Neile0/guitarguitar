eba6aa7b0f9c2be539924697d3b6d3ccb0fc2a4c
Andrew-Wyllie <77497575+Andrew-Wyllie@users.noreply.github.com>
2022-10-15 13:08:57 +0100

    Updated App.vue

--- guitarguitar/src/App.vue
+++ guitarguitar/src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div id="app">
+<<<<<<< HEAD:src/App.vue
+    <WhatDoYouLike/>
+=======
     <router-view />
+>>>>>>> e7ca60356b7f59bcca5dfa3a2066d805162315d2:guitarguitar/src/App.vue
   </div>
 </template>
 
 <script>
+<<<<<<< HEAD:src/App.vue
+import WhatDoYouLike from './components/WhatDoYouLike.vue';
+export default {
+  name: 'App',
+  components: {
+    WhatDoYouLike
+}
+=======
 //import ResultsScreen from './components/ResultsScreen.vue'
 export default {
   name: 'App',
   components: {
   
   }
+>>>>>>> e7ca60356b7f59bcca5dfa3a2066d805162315d2:guitarguitar/src/App.vue
 }
 </script>
 
@@ -22,7 +35,19 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+<<<<<<< HEAD:src/App.vue
+  margin-top: 60px;
+=======
   color: #2c3e50;
   margin-top: 0px;
+>>>>>>> e7ca60356b7f59bcca5dfa3a2066d805162315d2:guitarguitar/src/App.vue
+}
+html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+body {
+  background-color: rgb(70, 70, 70);
 }
 </style>
