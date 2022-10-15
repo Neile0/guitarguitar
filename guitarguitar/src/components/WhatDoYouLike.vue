35c4a258c43fc5d685cbce1e711573655deb001f
Andrew-Wyllie <77497575+Andrew-Wyllie@users.noreply.github.com>
2022-10-15 12:47:23 +0100

    What do you like and style sheet

--- guitarguitar/src/components/WhatDoYouLike.vue
+++ guitarguitar/src/components/WhatDoYouLike.vue
@@ -1,53 +1,20 @@
 <template> 
-    <div>
-        <h1>What Do You Like</h1>
-        <div @click="go('shape')" id="rectangle1">
-          <!--<a target="_blank" href="guitar.png">-->
-            <img :src="require('@/assets/guitar.png')" alt="guitar" width="200" height="400">
-          <!--</a>-->
-        </div>
-        <div @click="go('shape')" id="rectangle2">
-          <img :src="require('@/assets/guitar.png')" alt="guitar" width="200" height="400">
-        </div>
-        <div @click="go('shape')" id="rectangle3"></div>
-        
+<div>
+  <link href="stylesheet.css" rel="stylesheet"> 
+    <div id="rectangle1">
+      <img :src="require('@/assets/guitar.png')" alt="guitar" width="150" height="400">
+    </div>
+    <div id="rectangle2">
+      <img :src="require('@/assets/guitar.png')" alt="guitar" width="150" height="400">
+    </div>
+    <div id="rectangle3">
+      <img :src="require('@/assets/guitar.png')" alt="guitar" width="150" height="400">
+    </div>
     </div>
 </template>
-  <script>
-  export default {
-    name: 'WhatDoYouLike',
-    components: {
-        
-    },
-    props: ['media'],
-    data () {
-        return {
-        }
-    },
-    mounted () {
-      console.log(this.$route.params)
-    },
-    methods: {
-        go(t) {
-            this.$router.push({name:'resultsscreen',params:{"type":t,"media":this.media}})
-        },
-    }
-  }
-  </script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
   <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
   #rectangle1{
   border-radius: 25px;
   background: #767676;
