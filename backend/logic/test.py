aeb882c10500eff50c2a2d58987389df1df8976a
Aidan Neil <29099867+Neile0@users.noreply.github.com>
2022-10-15 15:35:37 +0100

    Adds endpoint for fetching all matching guitars given a media selection and filter (only by body shape for now)

---
+++ backend/logic/test.py
@@ -0,0 +1,6 @@
+import requests
+
+endpoint = "http://localhost:8000/api/media/"
+aJson = {"type":"shape","media":{"youtube":"https://www.youtube.com/embed/pAgnJDJN4VA" , "spotify":"https://open.spotify.com/embed/track/08mG3Y1vljYA6bvDt4Wqkj?si=fd4b4b4efce64148?autoplay=1&controls=0&mute=0" }} 
+
+requests.post(endpoint, json=aJson)
