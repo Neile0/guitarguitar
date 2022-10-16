d9f5ad2430c51b80a9c7c4c78d2bc1f68caccca7
Aidan Neil <29099867+Neile0@users.noreply.github.com>
2022-10-16 01:19:53 +0100

    Adds a (non-working) component for budget selection

---
+++ guitarguitar/src/components/BudgetScreen.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <h1>What is your budget?</h1>
+    
+    <div class="budgetSelector">
+    <p>100</p>
+    <RangeSelector/>
+    <p>3000</p>
+    </div>
+    <button>Get My Guitar</button>
+</div>
+</template>
+<script>
+import RangeSelector from './RangeSelector.vue'
+export default{
+name: 'BudgetScreen',
+component:{
+    RangeSelector,
+}
+}
+</script>
--- guitarguitar/src/router.js
+++ guitarguitar/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import ResultsScreen from './components/ResultsScreen.vue'
 import VideoScreen from './components/VideoScreen.vue'
 import WhatDoYouLike from './components/WhatDoYouLike.vue'
+import BudgetScreen from './components/BudgetScreen.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -24,6 +25,11 @@ export default new Router({
         name: 'resultsscreen',
         component: ResultsScreen,
         props:true
+      },
+      {
+        path: '/budget',
+        name: 'budgetscreen',
+        component: BudgetScreen,
       }
     ]
 });
